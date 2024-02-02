cc44938cde2ca9e5ec7f08c710089e221fc2e9a5
KHALIDCHOUHANGUJJAR <154968238+KHALIDCHOUHANGUJJAR@users.noreply.github.com>
2024-02-02 22:18:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display  = "block"
+})
+elemC.addEventListener("mouseleave",function(){
+  fixed.style.display  = "none"
+})
+
+var elems = document.querySelectorAll("#elem")
+elems.forEach(function(e){
+  e.addEventListener("mouseenter",function(){
+    var image = e.getAttribute("data-image")
+    fixed.style.background = `url(${image})`
+  })
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 0,
+   
+  });
+  var loader = document.querySelector("#loader")
+  setTimeout(function(){
+   loader.style.top ="-100%"
+  },4000)
